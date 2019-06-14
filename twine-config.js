a7f9a6dbd6703d73f6daea65880d500fe727f7fe
Mann Asugo <mannasugo@gmail.com>
2019-06-14 10:57:55 +0300

    Added twine database init variables

--- twine-config.js
+++ twine-config.js
@@ -9,6 +9,9 @@ module.exports = {
     d: twine,
   },
   SqlQuery: {
-    
+    twine: `create database if not exists twine`,
+    tempusers: `create table if not exists temp_users (altid TEXT not null,btime TEXT not null,chain VARCHAR(320) not null,idsum VARCHAR(320) not null,jtime TEXT not null,mail TEXT not null,reco VARCHAR(320) not null,referer VARCHAR(320) not null,sex TEXT not null)`,
+    users: `create table if not exists users (altid TEXT not null, btime TEXT not null, idsum VARCHAR(320) not null)`,
+    usermeta: `create table if not exists usermeta (bio TEXT not null,chain VARCHAR(320) not null,idsum VARCHAR(320) not null,mail TEXT not null,mug TEXT not null,pass TEXT not null)`,
   }
 }
