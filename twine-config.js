44884a77320d5da4b87b442d3c64af14074d277c
Mann Asugo <mannasugo@gmail.com>
2019-06-14 10:27:03 +0300

    Create twine-config.js

---
+++ twine-config.js
@@ -0,0 +1,14 @@
+let atrium = `public/gp`;
+
+module.exports = {
+  CSSDeck: `${atrium}/css`,
+  SQLPassConfig: {
+    h: localhost,
+    u: root,
+    p: Mann2asugo,
+    d: twine,
+  },
+  SqlQuery: {
+    
+  }
+}
