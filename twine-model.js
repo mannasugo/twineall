e63c43e5c4fcab320b275bd405889844dd44fdeb
Mann Asugo <mannasugo@gmail.com>
2019-07-13 19:01:41 +0300

    tritonal

--- twine-model.js
+++ twine-model.js
@@ -1,6 +1,34 @@
+class ModelString {
+  
+  constructor() {
+    this.appendString = ``;
+  }
+  
+  modelStringify (model) {
+    if (typeof model !== `object`) return;
+    model.forEach( function (miniModel) {
+      let a = miniModel.tag;
+      let z = a;
+      if (miniModel.tag_) a = miniModel.tag_;
+      this.appendString += `<` + a;
+      if (miniModel.flags) {
+        for (let flag in miniModel.flags) {
+          this.appendString += ` ` + flag + `='` + miniModel.flags[`flag`] + `'`;
+        }
+      }
+      this.appendString += `>`;
+      if (miniModel.closure) this.appendString += miniModel.closure;
+      if (miniModel.tagChild) this.modelStringify(miniModel.tagChild);
+      let queer = [`img`, `input`, `meta`];
+      if (queer.indexOf(miniModel.tag) === -1) this.appendString += `</` + z + `>`; 
+    });
+    return this.appendString;
+  }
+}
+
 module.exports = {
   
   callFrame (mapping) {
-    return this.ModelStringify(this.skeletal(mapping));
+    return this.modelStringify(this.skeletal(mapping));
   }
-}
+};
