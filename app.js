ea4397ed6dd1d91ab83281a6aaf2ee7273cf64d9
Mann Asugo <mannasugo@gmail.com>
2019-07-15 14:20:46 +0300

    Evanescence

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const fs = require(`fs`);
 const {router} = require(`./twine-control`);
 
 let app = createSecureServer({
-	key: fs.readFileSync(`http2/ssl/server.key`),
-	cert: fs.readFileSync(`http2/ssl.server.crt`),
-	allowHTTP1: true
+  key: fs.readFileSync(`http2/ssl/server.key`),
+  cert: fs.readFileSync(`http2/ssl.server.crt`),
+  allowHTTP1: true
 }, (req, res) => {
-	router(req, res);
+  router(req, res);
 });
 
 app.on(`error`, (err) => console.error(err));
