e43962ba1d75d3937ce9ca6dfb4ec4b9db259376
Mann Asugo <mannasugo@gmail.com>
2019-08-06 10:18:39 +0300

    UA->passValid: Validates password for login.

--- public/gp/js/twine-inactive.js
+++ public/gp/js/twine-inactive.js
@@ -1,5 +1,40 @@
 `use strict`;
 
+function passValid () {
+  const parentModel = document.querySelector(`form`);
+
+  parentModel.addEventListener(`submit`, e => e.preventDefault());
+
+  const listQModel = parentModel.querySelectorAll(`input`);
+
+  if (parentModel.querySelector(`button`).innerHTML !== `Sign in`) return;
+
+  parentModel.querySelector(`button`).addEventListener(`click`, () => {
+
+    const listOfValues = [];
+
+    for (let i = 0; i < listQModel.length; i++) {
+      let trimValue = new UAUtil().longMailTrim(listQModel[i].value);
+      if (!trimValue) return;
+      listOfValues[i] = trimValue;
+    }
+
+    if (listOfValues.length === 2) {
+      let UAStream = new UARequestRouter();
+      UAStream.URLEncodedCourier(`POST`, `/api/ua/`, {
+        title: `passValid`,
+        JSON: JSON.stringify({
+          mailTo: listOfValues[0],
+          mailPass: listOfValues[1]
+        });
+        handleRequest: () => {
+          if (UAStream.UAProto_.responseText.length < 1) return;
+        }
+      });
+    }
+  });
+}
+
 function electsAuthorize () {
   const eleUASubmit = document.querySelector(`._GXe > ._THa`);
 
